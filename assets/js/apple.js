402cd31fb9e25b93bf16e0d3b87c6e2895d68c5f
Caleb Wind (1000064480) <WINDCJ3@student.op.ac.nz>
2019-11-05 15:51:26 +1300

    Samsung and Apple JSON files are now JQUERY read in

--- assets/js/apple.js
+++ assets/js/apple.js
@@ -1,50 +1,51 @@
 window.onload = loadReviews = () => {
-  fetch("assets/json/apple.json") //pull the apple json file
- 
-    .then(r => r.json()) //make the input a json
-    .then(t => {
-      try {
-      let text =
-        "<table><tr><th>Product Name</th><th>Product Brand</th><th>Product Cost</th><th>Product Review</th><th>Rating</th></tr><tbody>";
-      Array.from(t).forEach(element => { //loop through all the json bits, you could also do the "text +=" all on one line but i feel this way is easier to read
-        text += `<tr><td>${element.itemName}</td>`;
-        text += `<td>${element.brand}</td>`;
-        text += `<td>${element.cost}</td>`;
-        text += `<td>${element.review}</td>`; //begin star html code
-        text += `<td><form class="rating" id="product1">
-        <button type="submit" class="star" data-star="1">
-          &#9733;
-          <span class="screen-reader">1 Star</span>
-        </button>
-      
-        <button type="submit" class="star" data-star="2">
-          &#9733;
-          <span class="screen-reader">2 Stars</span>
-        </button>
-      
-        <button type="submit" class="star" data-star="3">
-          &#9733;
-          <span class="screen-reader">3 Stars</span>
-        </button>
-      
-        <button type="submit" class="star" data-star="4">
-          &#9733;
-          <span class="screen-reader">4 Stars</span>
-        </button>
-      
-        <button type="submit" class="star" data-star="5">
-          &#9733;
-          <span class="screen-reader">5 Stars</span>
-        </button>
-      </form></td></tr>`; //this is the html code for the stars
-      });
-      text += "</tbody></table>";
-      document.getElementById("table").innerHTML += text;
-    }
-  catch (e) {
-console.log("e");
-  }});
-};
+  
+$(_ => {
+  $.get("assets/json/apple.json", data => {
+    try {
+            let text =
+              "<table><tr><th>Product Name</th><th>Product Brand</th><th>Product Cost</th><th>Product Review</th><th>Rating</th></tr><tbody>";
+            $.each(data, (idx, val) => {
+
+              text += `<tr><td>${val.itemName}</td>`;
+              text += `<td>${val.brand}</td>`;
+              text += `<td>${val.cost}</td>`;
+              text += `<td>${val.review}</td>`; //begin star html code
+              text += `<td><form class="rating" id="product1">
+              <button type="submit" class="star" data-star="1">
+                &#9733;
+                <span class="screen-reader">1 Star</span>
+              </button>
+            
+              <button type="submit" class="star" data-star="2">
+                &#9733;
+                <span class="screen-reader">2 Stars</span>
+              </button>
+            
+              <button type="submit" class="star" data-star="3">
+                &#9733;
+                <span class="screen-reader">3 Stars</span>
+              </button>
+            
+              <button type="submit" class="star" data-star="4">
+                &#9733;
+                <span class="screen-reader">4 Stars</span>
+              </button>
+            
+              <button type="submit" class="star" data-star="5">
+                &#9733;
+                <span class="screen-reader">5 Stars</span>
+              </button>
+            </form></td></tr>`; //this is the html code for the stars
+            });
+            text += "</tbody></table>";
+            document.getElementById("table").innerHTML += text;
+            }
+            
+          catch (e) {
+            console.log(e);
+              }});
+          });
 
 
 document.addEventListener("submit",function(event) { //https://gomakethings.com/converting-a-nodelist-to-an-array-with-vanilla-javascript/ code for stars, where i got it from 
@@ -91,4 +92,4 @@ document.addEventListener("click", event => {
       new Date().toLocaleString(); //paste the message
     document.getElementById("text-output").append(newDiv);
   }
-});
+})};
